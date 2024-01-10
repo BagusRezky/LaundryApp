303e07f23b4a12b99e4b180650b5460c7deeacc5
BagusRezky <bagusrezky15@gmail.com>
2024-01-10 22:12:04 +0700

    RestFull Api Read userid for laundry

--- dilaundry_backend/app/Http/Controllers/api/LaundryController.php
+++ dilaundry_backend/app/Http/Controllers/api/LaundryController.php
@@ -15,6 +15,29 @@ class LaundryController extends Controller
             // 'message' => 'Ini adalah readAll dari UserController',
             'data' => $laundries,
         ], 200);
+    }
 
+    function whereUserId($id)
+    {
+
+         $laundries = Laundry::where('user_id', '=' , $id)
+        ->with('user','shop')
+        ->orderBy('created_at', 'desc')
+        ->get();
+
+        if (count($laundries) > 0) {
+            return response()->json([
+                'message' => 'Berhasil menampilkan data',
+                'data' => $laundries
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Data tidak ditemukan',
+                'data' => $laundries,
+            ], 404);
+        }
+        return response()->json([
+            'data' => $laundries,
+        ], 200);
     }
 }
